7dc0a2068ca6be6a0d8b1ced6efea059699c3e92
Tri Putra Satriawan <tripsatriawan30@gmail.com>
2022-12-19 02:47:57 +0700

    add file ke 2

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:bank_front/shared/theme.dart';
+import 'package:bank_front/ui/widgets/home_service_item.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,47 +9,314 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: lightBackgroundColor,
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: blueColor,
-        unselectedItemColor: blackColor,
-        showSelectedLabels: true,
-        showUnselectedLabels: true,
-        selectedLabelStyle: blueTextStyle.copyWith(
-          fontSize: 10,
-          fontWeight: medium,
+      bottomNavigationBar: BottomAppBar(
+        color: whiteColor,
+        shape: const CircularNotchedRectangle(),
+        clipBehavior: Clip.antiAlias,
+        notchMargin: 6,
+        elevation: 0,
+        //script di atas untuk yang tanda plus agar floating dan membuat shape di bawah bottom navbarnya
+        child: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          //BottomNavigationBarType.fixed, berguna saat yang floating agar icon icon yang di bawah tidak tertutup
+          elevation: 0,
+          //elevation membuat tidak ada garis samar di bawah
+          backgroundColor: whiteColor,
+          selectedItemColor: blueColor,
+          //jika icon di klik ubah jadi biru
+          unselectedItemColor: blackColor,
+          //jika icon tidak di klik ubah jadi hitam
+          showSelectedLabels: true,
+          //menampikan selected labels
+          showUnselectedLabels: true,
+          //menampilkan un selected label
+          selectedLabelStyle: blueTextStyle.copyWith(
+            fontSize: 10,
+            fontWeight: medium,
+          ),
+          //untuk menampilkan text selected label
+          unselectedLabelStyle: blackTextStyle.copyWith(
+            fontSize: 10,
+            fontWeight: medium,
+          ),
+          //untuk menampikan text tidak ke select
+          items: [
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_overview.png',
+                width: 20,
+                color: blueColor,
+              ),
+              label: 'Overview',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_history.png',
+                width: 20,
+              ),
+              label: 'History',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_statistic.png',
+                width: 20,
+              ),
+              label: 'Statistic',
+            ),
+            BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_reward.png',
+                width: 20,
+              ),
+              label: 'Reward',
+            ),
+          ],
         ),
-        unselectedLabelStyle: blackTextStyle.copyWith(
-          fontSize: 10,
-          fontWeight: medium,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: purpleColor,
+        child: Image.asset(
+          'assets/ic_plus_circle.png',
+          width: 24,
         ),
-        items: [
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic_overview.png',
-              width: 20,
-            ),
-            label: 'Overview',
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: ListView(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        children: [
+          buildProfile(),
+          buildWalletCard(),
+          buildLevel(),
+          buildServicess(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildProfile() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 40,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Halo,',
+                style: grayTextStyle.copyWith(
+                  fontSize: 16,
+                ),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Text(
+                'Tri Putra Satriawan',
+                style: blackTextStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: semiBold,
+                ),
+              )
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic_history.png',
-              width: 20,
+          Container(
+            width: 60,
+            height: 60,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage(
+                  'assets/img_profile.png',
+                ),
+              ),
             ),
-            label: 'History',
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 16,
+                height: 16,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: whiteColor,
+                  //ini adalah script dimana icon di masukan kedalam container yang akan di tambah di belakang background nya menggunakan warna putih
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.check_circle,
+                    color: greenColor,
+                    size: 14,
+                    //icon ceklist
+                  ),
+                ),
+              ),
+            ),
           ),
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic_statistic.png',
-              width: 20,
-            ),
-            label: 'Statistic',
+        ],
+      ),
+    );
+  }
+
+  Widget buildWalletCard() {
+    return Container(
+      width: double.infinity,
+      height: 220,
+      margin: const EdgeInsets.only(
+        top: 30,
+      ),
+      padding: EdgeInsets.all(30),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(28),
+        image: const DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage(
+            'assets/img_bg_card.png',
           ),
-          BottomNavigationBarItem(
-            icon: Image.asset(
-              'assets/ic_reward.png',
-              width: 20,
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Tri Putra Satriawan',
+            style: whiteTextStyle.copyWith(
+              fontSize: 18,
+              fontWeight: medium,
             ),
-            label: 'Reward',
+          ),
+          const SizedBox(
+            height: 28,
+          ),
+          Text(
+            '**** **** **** **** 1280',
+            style: whiteTextStyle.copyWith(
+              fontSize: 18,
+              fontWeight: medium,
+              letterSpacing: 3,
+              //adalah untuk membuat jarak di setiap huruf
+            ),
+          ),
+          const SizedBox(
+            height: 21,
+          ),
+          Text(
+            'Balance',
+            style: whiteTextStyle,
+          ),
+          Text(
+            'Rp. 12.500',
+            style: whiteTextStyle.copyWith(
+              fontSize: 24,
+              fontWeight: semiBold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildLevel() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 20,
+      ),
+      padding: const EdgeInsets.all(22),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: whiteColor,
+      ),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text(
+                'Level 1',
+                style: blackTextStyle.copyWith(
+                  fontWeight: medium,
+                ),
+              ),
+              const Spacer(),
+              Text(
+                '55% ',
+                style: greenTextStyle.copyWith(
+                  fontWeight: semiBold,
+                ),
+              ),
+              Text(
+                'of Rp 20.000',
+                style: blackTextStyle.copyWith(
+                  fontWeight: semiBold,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          ClipRRect(
+            //cliprreact berguna untuk membungkus loading agar bisa circular dengan menambah border radius
+            borderRadius: BorderRadius.circular(55),
+            child: LinearProgressIndicator(
+              //bawwaan dari flutter untuk loading
+              value: 0.55, //adalah isi dari 55 % jadi 0.55
+              minHeight: 5,
+              valueColor: AlwaysStoppedAnimation(
+                  greenColor), // sintax supaya loading ga invinity
+              backgroundColor: lightBackgroundColor, //mengubah bgloadingnya
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildServicess() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 30,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Do Something',
+            style: blackTextStyle.copyWith(
+              fontWeight: semiBold,
+              fontSize: 16,
+            ),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              HomeServiceItem(
+                iconUrl: 'assets/ic_topup.png',
+                title: 'Top Up',
+                onTap: () {},
+              ),
+              HomeServiceItem(
+                iconUrl: 'assets/ic_send.png',
+                title: 'Send',
+                onTap: () {},
+              ),
+              HomeServiceItem(
+                iconUrl: 'assets/ic_withdraw.png',
+                title: 'Withdraw',
+                onTap: () {},
+              ),
+              HomeServiceItem(
+                iconUrl: 'assets/ic_more.png',
+                title: 'More',
+                onTap: () {},
+              ),
+            ],
           ),
         ],
       ),
